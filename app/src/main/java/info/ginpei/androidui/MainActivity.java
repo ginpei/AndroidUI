87a94d48affc037301548b9af20607b34639a3ed
ginpei <ginpei@gmail.com>
2017-01-23 03:28:07 -0800

    text to speech

--- app/src/main/java/info/ginpei/androidui/MainActivity.java
+++ app/src/main/java/info/ginpei/androidui/MainActivity.java
@@ -1,7 +1,9 @@
 package info.ginpei.androidui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_text_to_speech:
+                startActivity(new Intent(this, TextToSpeechActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/info/ginpei/androidui/TextToSpeechActivity.java
@@ -0,0 +1,50 @@
+package info.ginpei.androidui;
+
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class TextToSpeechActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+        tts = new TextToSpeech(this, this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_speech:
+                speak();
+                break;
+        }
+    }
+
+    public void speak() {
+        String text = ((EditText) findViewById(R.id.editText_textToBeSpeeched)).getText().toString();
+        speak(text);
+    }
+
+    public void speak(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.US);
+        }
+    }
+}

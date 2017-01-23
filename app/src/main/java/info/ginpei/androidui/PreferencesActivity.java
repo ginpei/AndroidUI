76dac81002aa73c563651eacd861406303092a3e
ginpei <ginpei@gmail.com>
2017-01-23 15:48:41 -0800

    display current value after changing

--- app/src/main/java/info/ginpei/androidui/PreferencesActivity.java
+++ app/src/main/java/info/ginpei/androidui/PreferencesActivity.java
@@ -1,7 +1,10 @@
 package info.ginpei.androidui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class PreferencesActivity extends AppCompatActivity {
@@ -16,11 +19,34 @@ public class PreferencesActivity extends AppCompatActivity {
                 .commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
+    public static class MyPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        private SharedPreferences preferences;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            preferences.registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            preferences.unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference pref = findPreference(key);
+            String value = preferences.getString(key, "");
+            pref.setSummary(value);
         }
     }
 }

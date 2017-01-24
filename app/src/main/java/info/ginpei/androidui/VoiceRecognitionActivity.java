db2d4936bcf64fe1f77e9add8d8d23260c41fc3f
ginpei <ginpei@gmail.com>
2017-01-24 13:08:01 -0800

    better message

--- app/src/main/java/info/ginpei/androidui/VoiceRecognitionActivity.java
+++ app/src/main/java/info/ginpei/androidui/VoiceRecognitionActivity.java
@@ -63,7 +63,7 @@ public class VoiceRecognitionActivity extends AppCompatActivity {
             listening = true;
             getPermission();
 
-            setStatusText("start recognition");
+            setStatusText("Initializing recognition...");
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
             intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "voice.recognition.test");

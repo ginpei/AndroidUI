b618b69e36e0343fef841a4d198f767c9a886fb3
ginpei <ginpei@gmail.com>
2017-01-27 15:40:31 -0800

    custom dialog

--- app/src/main/java/info/ginpei/androidui/DialogsActivity.java
+++ app/src/main/java/info/ginpei/androidui/DialogsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class DialogsActivity extends AppCompatActivity {
@@ -26,6 +27,13 @@ public class DialogsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openOkCancelDialog();
+            }
+        });
+
+        ((Button) findViewById(R.id.button_custom)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCustomDialog();
             }
         });
     }
@@ -57,6 +65,31 @@ public class DialogsActivity extends AppCompatActivity {
         dialog.setNegativeButton(android.R.string.cancel, null);
 
         dialog.create().show();
+    }
 
+    private void openCustomDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        dialog.setTitle("Add");
+        dialog.setView(R.layout.layout_custom_dialog);
+
+        dialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                AlertDialog alertDialog = (AlertDialog) dialog;
+                EditText nameEditText = (EditText) alertDialog.findViewById(R.id.editText_name);
+                String name = nameEditText.getText().toString();
+
+                if (name.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Canceled since required items are not given.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), name + " has been created.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        dialog.setNegativeButton(android.R.string.cancel, null);
+
+        dialog.create().show();
     }
 }

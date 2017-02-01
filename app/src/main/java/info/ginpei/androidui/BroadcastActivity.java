1f3af34c94f8d86308a71326180929f9fe721187
ginpei <ginpei@gmail.com>
2017-01-31 22:36:00 -0800

    split receiver; one's types are defined in manifest, the other's are defined in code

--- app/src/main/java/info/ginpei/androidui/BroadcastActivity.java
+++ app/src/main/java/info/ginpei/androidui/BroadcastActivity.java
@@ -4,18 +4,22 @@
 
 package info.ginpei.androidui;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BroadcastActivity extends AppCompatActivity {
 
-    PowerConnectionReceiver receiver = new PowerConnectionReceiver();
+    LocalPowerConnectionReceiver receiver = new LocalPowerConnectionReceiver();
     private TextView statusTextView;
+    private ImageView chargingImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +27,24 @@ public class BroadcastActivity extends AppCompatActivity {
         setContentView(R.layout.activity_broadcast);
 
         statusTextView = (TextView) findViewById(R.id.textView_status);
+        chargingImageView = (ImageView) findViewById(R.id.imageView_charging);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        receiver.setListener(new PowerConnectionReceiver.OnReceiveListener() {
+        receiver.setListener(new LocalPowerConnectionReceiver.OnReceiveListener() {
             @Override
-            public void onReceive(Context context, Intent intent) {
-                String status;
-                String action = intent.getAction();
-                if (action == "android.intent.action.ACTION_POWER_CONNECTED") {
-                    status = "connected";
-                } else {
-                    status = "disconnected";
-                }
-                statusTextView.setText("Power is " + status);
+            public void onReceive(Context context, final Intent intent) {
+                // run on UI thread to allow handle UI
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String action = intent.getAction();
+                        BroadcastActivity.this.onReceive(action);
+                    }
+                });
             }
         });
 
@@ -49,10 +54,57 @@ public class BroadcastActivity extends AppCompatActivity {
         this.registerReceiver(receiver, filter);
     }
 
+    private void onReceive(String action) {
+        String status;
+        int bgColor;
+        if (action.equals("android.intent.action.ACTION_POWER_CONNECTED")) {
+            status = "connected";
+            bgColor = android.R.color.holo_green_dark;
+        } else if (action.equals("android.intent.action.ACTION_POWER_DISCONNECTED")) {
+            status = "disconnected";
+            bgColor = android.R.color.darker_gray;
+        } else {
+            Log.d("BroadcastActivity", action);
+            status = null;
+            bgColor = android.R.color.white;
+        }
+
+        String message;
+        if (status != null) {
+            message = "Power is " + status;
+        } else {
+            message = "Ready. Try plug on and off now.";
+        }
+        statusTextView.setText(message);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            chargingImageView.setBackgroundColor(getColor(bgColor));
+        }
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
 
         this.unregisterReceiver(receiver);
+    }
+
+    public static class LocalPowerConnectionReceiver extends BroadcastReceiver {
+
+        protected OnReceiveListener listener = null;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (listener != null) {
+                listener.onReceive(context, intent);
+            }
+        }
+
+        public void setListener(OnReceiveListener listener) {
+            this.listener = listener;
+        }
+
+        public interface OnReceiveListener {
+            void onReceive(Context context, Intent intent);
+        }
     }
 }
--- app/src/main/java/info/ginpei/androidui/PowerConnectionReceiver.java
+++ app/src/main/java/info/ginpei/androidui/PowerConnectionReceiver.java
@@ -9,28 +9,14 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
 
     public static final String TAG = "PowerConnectionReceiver";
 
-    protected OnReceiveListener listener = null;
-
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         String uri = intent.toUri(Intent.URI_INTENT_SCHEME).toString();
         log("Action: " + action + ", URI: " + uri);
-
-        if (listener != null) {
-            listener.onReceive(context, intent);
-        }
-    }
-
-    public void setListener(OnReceiveListener listener) {
-        this.listener = listener;
     }
 
     public void log(String text) {
         Log.d(TAG, text);
-    }
-
-    public static interface OnReceiveListener {
-        void onReceive(Context context, Intent intent);
     }
 }

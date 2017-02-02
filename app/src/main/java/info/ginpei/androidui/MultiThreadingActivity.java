fed65dff2de1604d305e44f27aed00290b1f3d58
ginpei <ginpei@gmail.com>
2017-02-02 14:06:22 -0800

    do something in the other thread

--- app/src/main/java/info/ginpei/androidui/MultiThreadingActivity.java
+++ app/src/main/java/info/ginpei/androidui/MultiThreadingActivity.java
@@ -2,6 +2,8 @@ package info.ginpei.androidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MultiThreadingActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MultiThreadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_threading);
+
+        ((Button) findViewById(R.id.button_start)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+    }
+
+    private void start() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 300000000; i++) {
+                    if (i % 10000000 == 0) {
+                        System.out.println(i);
+                    }
+                }
+                System.out.println("Done!");
+            }
+        };
+
+        thread.start();
     }
 }

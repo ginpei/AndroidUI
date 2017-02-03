7309807f654d10a9007897b2481cf6046103c28e
ginpei <ginpei@gmail.com>
2017-02-03 15:21:49 -0800

    play music

--- app/src/main/java/info/ginpei/androidui/PlayMusicActivity.java
+++ app/src/main/java/info/ginpei/androidui/PlayMusicActivity.java
@@ -1,13 +1,31 @@
 package info.ginpei.androidui;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PlayMusicActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mediaPlayer.stop();
     }
 }

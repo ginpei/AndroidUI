8fae63b4935f93c1eb1f1523c5e6c335c94e27d2
ginpei <ginpei@gmail.com>
2017-02-03 16:22:15 -0800

    track progress

--- app/src/main/java/info/ginpei/androidui/PlayMusicActivity.java
+++ app/src/main/java/info/ginpei/androidui/PlayMusicActivity.java
@@ -1,22 +1,41 @@
 package info.ginpei.androidui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class PlayMusicActivity extends AppCompatActivity {
 
+    public static final String ACTION_TICK = "info.ginpei.androidui.PlayMusicActivity.Ticker.TICK";
+    public static final int TICKER_INTERVAL = 100;
+
     private MediaPlayer mediaPlayer;
     private ToggleButton playPauseToggleButton;
+    private AsyncTask ticker;
+    private BroadcastReceiver receiver;
+    private TextView progressTextView;
+    private TextView durationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+
+        buildTicker();
+
         playPauseToggleButton = (ToggleButton) findViewById(R.id.button_playPause);
+        progressTextView = (TextView) findViewById(R.id.textView_progress);
+        durationTextView = (TextView) findViewById(R.id.textView_duration);
 
         playPauseToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -28,25 +47,63 @@ public class PlayMusicActivity extends AppCompatActivity {
                 }
             }
         });
-
-        mediaPlayer = MediaPlayer.create(this, R.raw.music);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void buildTicker() {
+        ticker = new AsyncTask() {
+            @Override
+            protected Void doInBackground(Object... values) {
+                while (true) {
+                    sendBroadcast(new Intent(ACTION_TICK));
+
+                    try {
+                        Thread.sleep(TICKER_INTERVAL);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (action.equals(ACTION_TICK)) {
+                    updateProgress();
+                }
+            }
+        };
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        pause();
     }
 
     private void play() {
         mediaPlayer.start();
+        durationTextView.setText(String.valueOf(mediaPlayer.getDuration()));
+        startTicker();
     }
 
     private void pause() {
         mediaPlayer.pause();
+        stopTicker();
+    }
+
+    private void startTicker() {
+        ticker.execute();
+        registerReceiver(receiver, new IntentFilter(ACTION_TICK));
+    }
+
+    private void stopTicker() {
+        unregisterReceiver(receiver);
+        ticker.cancel(true);
+    }
+
+    private void updateProgress() {
+        progressTextView.setText(String.valueOf(mediaPlayer.getCurrentPosition()));
     }
 }

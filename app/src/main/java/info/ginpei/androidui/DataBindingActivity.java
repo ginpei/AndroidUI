609cd4a344f23ab9d0a468eca6dc257f5c614e4e
ginpei <ginpei@gmail.com>
2017-02-21 16:33:56 -0800

    data binding

--- app/src/main/java/info/ginpei/androidui/DataBindingActivity.java
+++ app/src/main/java/info/ginpei/androidui/DataBindingActivity.java
@@ -1,13 +1,48 @@
 package info.ginpei.androidui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import info.ginpei.androidui.databinding.ActivityDataBindingBinding;
 
 public class DataBindingActivity extends AppCompatActivity {
+
+    public static final String TAG = "G#DataBindingActivity";
+    public ViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_binding);
+
+        vm = new ViewModel();
+
+        ActivityDataBindingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding);
+        binding.setActivity(this);
+        binding.setVm(vm);
+    }
+
+    public void updateButton_click(View view) {
+        Log.d(TAG, "onClick() called");
+        vm.setMessage("Hello Updated World!!");
+    }
+
+    public class ViewModel extends BaseObservable {
+
+        public String message = "Hello!";
+
+        @Bindable
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+            notifyPropertyChanged(BR.message);
+        }
     }
 }
